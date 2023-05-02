dd295b0008b68ebc57458096ff8de1e7fc29e416
ridhocr <ridhocindra21.com>
2023-05-02 16:57:07 +0700

    TESSSSSSSSSS

---
+++ TES/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TES.Startup))]
+namespace TES
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
